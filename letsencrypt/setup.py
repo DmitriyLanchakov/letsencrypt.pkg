d6084dac87d0231d72bd291227035a23b035ab19
Anton Batenev <antonbatenev@yandex.ru>
2015-12-09 03:46:00 +0300

    Add initial letsencrypt dh_virtualenv

---
+++ letsencrypt/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='letsencrypt',
+      version='0.1.0',
+      author=u'Anton Batenev',
+      author_email='antonbatenev@yandex.ru',
+      url='https://github.com/letsencrypt/letsencrypt.git',
+      description='Dummy package to make dh_virtualenv work',
+)
