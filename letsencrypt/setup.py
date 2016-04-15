6865339210c6c89702a0ec8d03c3bac96ef0dc31
Anton Batenev <antonbatenev@yandex.ru>
2016-04-15 12:57:33 +0300

    Update to v0.5.0

--- letsencrypt/setup.py
+++ letsencrypt/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='letsencrypt',
-      version='0.4.2',
+      version='0.5.0',
       author=u'Anton Batenev',
       author_email='antonbatenev@yandex.ru',
       url='https://github.com/letsencrypt/letsencrypt.git',
